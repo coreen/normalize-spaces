8024b85d20bca230118d67915895de7df8ec8d96
coreen <coreeny@cs.washington.edu>
2017-11-20 07:53:26 -0800

    reverse of tab-to-space conversion

--- README.txt
+++ README.txt
@@ -1,4 +1,5 @@
 Generate executable via
 ```
 gcc -Wall -g -std=c11 -o tabReplace tabReplace.c
+gcc -Wall -g -std=c11 -o spaceReplace spaceReplace.c
 ```
--- spaceReplace.c
+++ spaceReplace.c
@@ -1,5 +1,5 @@
 /*
-replaces TAB characters in user input with spaces
+replace spaces in user input with TAB characters
 */
 
 #include<stdio.h>
@@ -13,10 +13,13 @@ int main(void)
     int value = 0;
     // counter variable counts the number of spaces read from user input
     int counter = 0;
-    // x , y, and z variables are used in the for loops
+    // w, x , y, and z variables are used in the for loops
+    int w = 0;
     int x = 0;
     int y = 0;
     int z = 0;
+    // drop variable keeps track of whether or not to print trailing spaces
+    char drop = '\0';
     // flag variable keeps track of when to print ending TAB/space marker
     int flag = 0;
 
@@ -27,8 +30,22 @@ int main(void)
     // throwaway newline for tabSize entry
     getchar();
 
+    // asks the user whether or not to drop trailing spaces after tab conversion
+    printf("do you want trailing spaces after TAB conversion to be dropped? [Y/N]: \n");
+    drop = getchar();
+    if (drop  == 'Y') {
+        printf("dropping trailing spaces\n");
+    } else {
+        if (drop != 'N') {
+            printf("invalid response entered, leaving spaces in conversion\n");
+        }
+        printf("leaving spaces in conversion requested\n");
+    }
+    // throwaway newline for drop entry
+    getchar();
+
     // asks the user for any number of spaces until the user enters a newline
-    printf("please enter anything with TAB characters and spaces (ENTER to process): \n");
+    printf("please enter anything with many TAB characters and spaces (ENTER to process): \n");
     value = getchar();
     while (value != '\n') {
         // counts the character accordingly if it is a space character
@@ -42,8 +59,13 @@ int main(void)
         // printing information to the user through stdout
         // prints the remaining spaces and non-space character read
         } else {
-            for (x = 0; x < counter; x++) {
-                printf(" ");
+            for (w = 0; w < (counter / tabSize); w++) {
+                printf("\t");
+            }
+            if (drop == 'Y') {
+                for (x = 0; x < (counter % tabSize); x++) {
+                    printf(" ");
+                }
             }
             counter = 0;
             // tests if an ending marker needs to be printed
@@ -56,10 +78,13 @@ int main(void)
     }
     // print any remaining TABs and spaces after last non-space character
     for (y = 0; y < (counter / tabSize); y++) {
-        printf(" ");
+        printf("\t");
     }
-    for (z = 0; z < (counter % tabSize); z++) {
-        printf(" ");
+    // trailing spaces
+    if (drop == 'Y') {
+        for (z = 0; z < (counter % tabSize); z++) {
+            printf(" ");
+        }
     }
     printf("\n");
 
